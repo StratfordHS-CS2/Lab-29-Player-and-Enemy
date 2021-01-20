4113d2d9a714c2f43dcf29778aff4597ac6ce0b5
Dave Avis <charles.avis@springbranchisd.com>
2021-01-20 13:47:32 -0600

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -13,29 +13,29 @@ class Enemy:
 	It should also set the Player's attack_dmg to be 5.
 	'''
 	def __init__():
-    pass
+    		pass
 	
 	'''
 	This method should receive a parameter that is the object that is the target of the attack.
 	Call the hit method for that object and send it the amount of damage caused.
 	'''
 	def attack():
-    pass
+    		pass
 	
 	'''
 	Print a vengeful message that the Enemy has died.
 	'''
 	def die():
-    pass
+    		pass
 	
 	'''
 	Receive damage from an attack.  Reduce hp by dmg, print an updated health status, die if out of hp.
 	'''
 	def hit():
-    pass
+    		pass
 			
 	'''
 	return a message that includes the Enemy's name and health status.
 	'''
 	def __str__():
-    pass
+    		pass
